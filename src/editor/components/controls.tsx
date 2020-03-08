9c06c24793beca197bb8a269fcb24b421804a1e2
Kevin Trieu <dragn194@gmail.com>
2020-03-08 16:09:06 -0400

    Add a popover to configure links

--- src/editor/components/controls.tsx
+++ src/editor/components/controls.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Mark } from "../types";
 import { useSlate } from "slate-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,7 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { toggleMark, isMarkActive, toggleLink } from "../helpers";
 
 import "./controls.scss";
+import LinkPopover from "./LinkPopover";
 
 interface ToggleMarkButtonProps {
   icon: string;
@@ -32,14 +33,34 @@ export const ToggleMarkButton: React.FC<ToggleMarkButtonProps> = (
 
 export const LinkButton: React.FC = () => {
   const editor = useSlate();
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const [showPopover, setShowPopover] = useState<boolean>(false);
+
+  const setHref = (href: string) => {
+    setShowPopover(false);
+    toggleLink(editor, href);
+  };
+
   return (
-    <button
-      className="editor-control"
-      onClick={() => {
-        toggleLink(editor, "google.com");
-      }}
-    >
-      <FontAwesomeIcon icon="link" />
-    </button>
+    <>
+      <button
+        ref={ref}
+        className="editor-control"
+        onClick={() => {
+          setShowPopover(true);
+        }}
+      >
+        <FontAwesomeIcon icon="link" />
+      </button>
+      {ref.current && (
+        <LinkPopover
+          target={ref.current}
+          href=""
+          show={showPopover}
+          setHref={setHref}
+        />
+      )}
+    </>
   );
 };
