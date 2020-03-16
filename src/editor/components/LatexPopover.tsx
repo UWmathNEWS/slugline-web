c43330ea161009b0ac2b4697b1d62bbee883abdc
Kevin Trieu <ktrieu@secretmission.io>
2020-03-16 00:26:46 -0400

    Some style tweaks to the editor

--- src/editor/components/LatexPopover.tsx
+++ src/editor/components/LatexPopover.tsx
@@ -12,24 +12,26 @@ const LatexPopover: React.FC<LatexPopoverProps> = (
   const [latex, setLatex] = useState<string>(props.latex);
 
   return (
-    <Form
-      onSubmit={(evt: React.FormEvent) => {
-        evt.preventDefault();
-        props.setLatex(latex);
-      }}
-    >
-      <Form.Group controlId="latex">
-        <Form.Label>LaTeX:</Form.Label>
-        <Form.Control
-          type="text"
-          value={latex}
-          onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
-            setLatex(evt.currentTarget.value);
-          }}
-        />
-      </Form.Group>
-      <Button type="submit">Done</Button>
-    </Form>
+    <div className="control-popover">
+      <Form
+        onSubmit={(evt: React.FormEvent) => {
+          evt.preventDefault();
+          props.setLatex(latex);
+        }}
+      >
+        <Form.Group controlId="latex">
+          <Form.Label>LaTeX:</Form.Label>
+          <Form.Control
+            type="text"
+            value={latex}
+            onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
+              setLatex(evt.currentTarget.value);
+            }}
+          />
+        </Form.Group>
+        <Button type="submit">Done</Button>
+      </Form>
+    </div>
   );
 };
 
