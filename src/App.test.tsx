4c0bfc03a77c82d7a572eba52bf5e4c13959dbef
Vincent Macri <developer@vincemacri.ca>
2020-05-10 15:37:35 -0400

    Add useful test (#16)
    * Add useful test
    
    * Address PR feedback

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("render is not null ", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText).not.toBeNull();
 });
