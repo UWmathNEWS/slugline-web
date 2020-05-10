3f39c8cb2792a57b522439ff0fee460fa980d293
Vincent Macri <developer@vincemacri.ca>
2020-05-10 15:34:37 -0400

    Address PR feedback

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("render is not null ", () => {
   const { getByText } = render(<App />);
   expect(getByText).not.toBeNull();
 });
