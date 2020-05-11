e85313c02adcbb5620a46d8d78d4a455ce6c3f19
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-05-10 22:27:12 -0400

    Add back success alerts and non field errors

--- src/profile/ProfileForm.tsx
+++ src/profile/ProfileForm.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { User, APIResponse, UserAPIError } from "../shared/types";
-import { Form, Row, Col, Button, InputGroup } from "react-bootstrap";
+import { Form, Row, Col, Button, Alert } from "react-bootstrap";
 import { FormContextValues, useForm } from "react-hook-form";
 import Field from "../shared/form/Field";
 
@@ -12,6 +12,7 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import nanoid from "nanoid";
 import { cleanFormData } from "../shared/form/util";
 import { useDebouncedCallback } from "../shared/hooks";
+import NonFieldErrors from "../shared/form/NonFieldErrors";
 
 export interface ProfileFormVals {
   username?: string;
@@ -65,6 +66,13 @@ export const ProfileFormConsumer: React.FC<ProfileConsumerFormProps> = (
 
   const { reset, register } = props.context;
 
+  const [successMessage, setSuccessMessage] = useState<string | undefined>(
+    undefined
+  );
+  const [generalErrors, setGeneralErrors] = useState<string[] | undefined>(
+    undefined
+  );
+
   // react-hook-form caches default values when the first time you call useForm.
   // So, we have to manually replicate the defaultValues behaviour by resetting the form values ourselves
   // whenever user changes
@@ -100,6 +108,7 @@ export const ProfileFormConsumer: React.FC<ProfileConsumerFormProps> = (
     try {
       if (props.user === undefined) {
         await auth.post<ProfileFormVals>("users/", cleaned);
+        setSuccessMessage(`User ${vals.username} created successfully.`);
         // refresh the form so we can add a new user
         props.context.reset();
       } else {
@@ -108,7 +117,9 @@ export const ProfileFormConsumer: React.FC<ProfileConsumerFormProps> = (
           cleaned,
           editingMe
         );
+        setSuccessMessage(`User ${vals.username} saved successfully.`);
       }
+      setGeneralErrors(undefined);
       if (props.onSubmitSuccessful) {
         await props.onSubmitSuccessful(vals);
       }
@@ -128,6 +139,8 @@ export const ProfileFormConsumer: React.FC<ProfileConsumerFormProps> = (
           message: error,
         })) || []
       );
+      setSuccessMessage(undefined);
+      setGeneralErrors(apiErrors.detail);
       if (props.onSubmitFailed) {
         await props.onSubmitFailed(vals, apiErrors);
       }
@@ -135,236 +148,252 @@ export const ProfileFormConsumer: React.FC<ProfileConsumerFormProps> = (
   };
 
   return (
-    <Form id={props.formId} onSubmit={props.context.handleSubmit(submit)}>
-      <Form.Group as={Row} controlId="username">
-        <Form.Label column sm={2}>
-          Username
-        </Form.Label>
-        <Col sm={10}>
-          <Field
-            errors={props.context.errors}
-            type="text"
-            name="username"
-            disabled={props.user !== undefined}
-            validMessage={"This username is valid."}
-            isValid={
-              props.context.formState.dirtyFields.has("username") &&
-              !props.context.errors["username"]
-            }
-            ref={props.context.register({
-              maxLength: {
-                value: 150,
-                message: "USER.USERNAME.TOO_LONG",
-              },
-              required: true,
-              validate: async (username: string) => {
-                if (props.user !== undefined) {
-                  // if we're editing a user the field is disabled anyway, don't validate
-                  return;
-                }
-                if (!(await validateUserNameDebounced(username))) {
-                  return Promise.resolve("USER.USERNAME.ALREADY_EXISTS");
-                }
-              },
-            })}
-          />
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row}>
-        <Form.Label column sm={2}>
-          Name
-        </Form.Label>
-        <Col sm={5}>
-          <Field
-            errors={props.context.errors}
-            type="text"
-            name="first_name"
-            placeholder="First Name"
-            ref={props.context.register({
-              maxLength: {
-                value: 30,
-                message: "USER.FIRST_NAME.TOO_LONG.30",
-              },
-              required: true,
-            })}
-          />
-        </Col>
-        <Col sm={5}>
-          <Field
-            errors={props.context.errors}
-            type="text"
-            name="last_name"
-            placeholder="Last Name"
-            ref={props.context.register({
-              maxLength: {
-                value: 150,
-                message: "USER.LAST_NAME.TOO_LONG.150",
-              },
-            })}
-          />
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} controlId="email">
-        <Form.Label column sm={2}>
-          Email
-        </Form.Label>
-        <Col sm={10}>
-          <Field
-            errors={props.context.errors}
-            type="email"
-            name="email"
-            ref={props.context.register({
-              required: "USER.REQUIRED.email",
-              maxLength: {
-                value: 254,
-                message: "USER.EMAIL.TOO_LONG.254",
-              },
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "USER.EMAIL.INVALID",
-              },
-            })}
-          />
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} controlId="writerName">
-        <Form.Label column sm={2}>
-          Writer Name
-        </Form.Label>
-        <Col sm={10}>
-          <Field
-            errors={props.context.errors}
-            type="text"
-            name="writer_name"
-            ref={props.context.register({
-              required: "USER.REQUIRED.name",
-            })}
-          />
-        </Col>
-      </Form.Group>
-      {auth.isEditor() && (
-        <Form.Group as={Row} controlId="isEditor">
+    <>
+      {successMessage && (
+        <Alert
+          variant="success"
+          dismissible
+          onClose={() => {
+            setSuccessMessage(undefined);
+          }}
+        >
+          {successMessage}
+        </Alert>
+      )}
+      <Form id={props.formId} onSubmit={props.context.handleSubmit(submit)}>
+        <Form.Group as={Row} controlId="username">
           <Form.Label column sm={2}>
-            Role
+            Username
           </Form.Label>
           <Col sm={10}>
-            <Form.Control
-              as="select"
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                if (e.currentTarget.value === "editor") {
-                  props.context.setValue("is_editor", true);
-                } else if (e.currentTarget.value === "contributor") {
-                  props.context.setValue("is_editor", false);
-                }
-              }}
-              value={
-                props.context.getValues().is_editor ? "editor" : "contributor"
+            <Field
+              errors={props.context.errors}
+              type="text"
+              name="username"
+              disabled={props.user !== undefined}
+              validMessage={"This username is valid."}
+              isValid={
+                props.context.formState.dirtyFields.has("username") &&
+                !props.context.errors["username"]
               }
-              className="custom-select"
-              custom
-            >
-              <option value="contributor">Contributor</option>
-              <option value="editor">Editor</option>
-            </Form.Control>
+              ref={props.context.register({
+                maxLength: {
+                  value: 150,
+                  message: "USER.USERNAME.TOO_LONG",
+                },
+                required: true,
+                validate: async (username: string) => {
+                  if (props.user !== undefined) {
+                    // if we're editing a user the field is disabled anyway, don't validate
+                    return;
+                  }
+                  if (!(await validateUserNameDebounced(username))) {
+                    return Promise.resolve("USER.USERNAME.ALREADY_EXISTS");
+                  }
+                },
+              })}
+            />
           </Col>
         </Form.Group>
-      )}
-      {(props.user !== undefined && !auth.user?.is_editor) ||
-        (props.user?.username === auth.user?.username && (
-          <>
-            <h3>Change Password</h3>
-            <Form.Group as={Row} controlId="curPassword">
-              <Form.Label column sm={2}>
-                Current Password
-              </Form.Label>
-              <Col sm={10}>
-                <Field
-                  errors={props.context.errors}
-                  type="password"
-                  name="cur_password"
-                  ref={props.context.register({
-                    validate: (password: string) => {
-                      if (
-                        props.user !== undefined &&
-                        props.context.getValues().password &&
-                        !props.context.getValues().cur_password
-                      ) {
-                        return "USER.PASSWORD.CURRENT_REQUIRED";
-                      }
-                    },
-                  })}
-                />
-              </Col>
-            </Form.Group>
-          </>
-        ))}
-      <Form.Group as={Form.Row} controlId="password">
-        <Form.Label column sm={2}>
-          New Password
-        </Form.Label>
-        <Col sm={2}>
-          <Button
-            className="w-100"
-            variant="secondary"
-            onClick={() => {
-              props.context.setValue("password", nanoid());
-              // set the password visible so you can see what you get
-              setShowPassword(true);
-            }}
-          >
-            Generate
-          </Button>
-        </Col>
-        <Col sm={8}>
-          <Field
-            errors={props.context.errors}
-            type={showPassword ? "text" : "password"}
-            name="password"
-            ref={props.context.register({
-              minLength: {
-                value: 8,
-                message: "USER.PASSWORD.TOO_SHORT.8",
-              },
-              validate: (password?: string) => {
-                // the pattern argument doesn't let us return an error if the value FAILS a regex,
-                // so we'll do it ourselves
-                if (password && /^\d*$/.test(password)) {
-                  return "USER.PASSWORD.ENTIRELY_NUMERIC";
-                }
-                if (props.context.getValues().cur_password && !password) {
-                  return "USER.PASSWORD.NEW_REQUIRED";
-                }
-              },
-            })}
-            append={
-              <Button
-                variant={showPassword ? "outline-primary" : "outline-secondary"}
-                onClick={() => {
-                  setShowPassword((show) => !show);
+        <Form.Group as={Row}>
+          <Form.Label column sm={2}>
+            Name
+          </Form.Label>
+          <Col sm={5}>
+            <Field
+              errors={props.context.errors}
+              type="text"
+              name="first_name"
+              placeholder="First Name"
+              ref={props.context.register({
+                maxLength: {
+                  value: 30,
+                  message: "USER.FIRST_NAME.TOO_LONG.30",
+                },
+                required: true,
+              })}
+            />
+          </Col>
+          <Col sm={5}>
+            <Field
+              errors={props.context.errors}
+              type="text"
+              name="last_name"
+              placeholder="Last Name"
+              ref={props.context.register({
+                maxLength: {
+                  value: 150,
+                  message: "USER.LAST_NAME.TOO_LONG.150",
+                },
+              })}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="email">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Field
+              errors={props.context.errors}
+              type="email"
+              name="email"
+              ref={props.context.register({
+                required: "USER.REQUIRED.email",
+                maxLength: {
+                  value: 254,
+                  message: "USER.EMAIL.TOO_LONG.254",
+                },
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "USER.EMAIL.INVALID",
+                },
+              })}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="writerName">
+          <Form.Label column sm={2}>
+            Writer Name
+          </Form.Label>
+          <Col sm={10}>
+            <Field
+              errors={props.context.errors}
+              type="text"
+              name="writer_name"
+              ref={props.context.register({
+                required: "USER.REQUIRED.name",
+              })}
+            />
+          </Col>
+        </Form.Group>
+        {auth.isEditor() && (
+          <Form.Group as={Row} controlId="isEditor">
+            <Form.Label column sm={2}>
+              Role
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                as="select"
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                  if (e.currentTarget.value === "editor") {
+                    props.context.setValue("is_editor", true);
+                  } else if (e.currentTarget.value === "contributor") {
+                    props.context.setValue("is_editor", false);
+                  }
                 }}
+                value={
+                  props.context.getValues().is_editor ? "editor" : "contributor"
+                }
+                className="custom-select"
+                custom
               >
-                {showPassword ? (
-                  <FontAwesomeIcon icon={faEyeSlash} />
-                ) : (
-                  <FontAwesomeIcon icon={faEye} />
-                )}
-              </Button>
+                <option value="contributor">Contributor</option>
+                <option value="editor">Editor</option>
+              </Form.Control>
+            </Col>
+          </Form.Group>
+        )}
+        {(props.user !== undefined && !auth.user?.is_editor) ||
+          (props.user?.username === auth.user?.username && (
+            <>
+              <h3>Change Password</h3>
+              <Form.Group as={Row} controlId="curPassword">
+                <Form.Label column sm={2}>
+                  Current Password
+                </Form.Label>
+                <Col sm={10}>
+                  <Field
+                    errors={props.context.errors}
+                    type="password"
+                    name="cur_password"
+                    ref={props.context.register({
+                      validate: (password: string) => {
+                        if (
+                          props.user !== undefined &&
+                          props.context.getValues().password &&
+                          !props.context.getValues().cur_password
+                        ) {
+                          return "USER.PASSWORD.CURRENT_REQUIRED";
+                        }
+                      },
+                    })}
+                  />
+                </Col>
+              </Form.Group>
+            </>
+          ))}
+        <Form.Group as={Form.Row} controlId="password">
+          <Form.Label column sm={2}>
+            New Password
+          </Form.Label>
+          <Col sm={2}>
+            <Button
+              className="w-100"
+              variant="secondary"
+              onClick={() => {
+                props.context.setValue("password", nanoid());
+                // set the password visible so you can see what you get
+                setShowPassword(true);
+              }}
+            >
+              Generate
+            </Button>
+          </Col>
+          <Col sm={8}>
+            <Field
+              errors={props.context.errors}
+              type={showPassword ? "text" : "password"}
+              name="password"
+              ref={props.context.register({
+                minLength: {
+                  value: 8,
+                  message: "USER.PASSWORD.TOO_SHORT.8",
+                },
+                validate: (password?: string) => {
+                  // the pattern argument doesn't let us return an error if the value FAILS a regex,
+                  // so we'll do it ourselves
+                  if (password && /^\d*$/.test(password)) {
+                    return "USER.PASSWORD.ENTIRELY_NUMERIC";
+                  }
+                  if (props.context.getValues().cur_password && !password) {
+                    return "USER.PASSWORD.NEW_REQUIRED";
+                  }
+                },
+              })}
+              append={
+                <Button
+                  variant={
+                    showPassword ? "outline-primary" : "outline-secondary"
+                  }
+                  onClick={() => {
+                    setShowPassword((show) => !show);
+                  }}
+                >
+                  {showPassword ? (
+                    <FontAwesomeIcon icon={faEyeSlash} />
+                  ) : (
+                    <FontAwesomeIcon icon={faEye} />
+                  )}
+                </Button>
+              }
+            />
+          </Col>
+        </Form.Group>
+        <NonFieldErrors errors={generalErrors || []} />
+        {!props.hideSubmit && (
+          <Button
+            type="submit"
+            disabled={
+              props.context.formState.isSubmitting ||
+              !props.context.formState.isValid
             }
-          />
-        </Col>
-      </Form.Group>
-      {!props.hideSubmit && (
-        <Button
-          type="submit"
-          disabled={
-            props.context.formState.isSubmitting ||
-            !props.context.formState.isValid
-          }
-        >
-          {props.context.formState.isSubmitting ? "Saving..." : "Save"}
-        </Button>
-      )}
-    </Form>
+          >
+            {props.context.formState.isSubmitting ? "Saving..." : "Save"}
+          </Button>
+        )}
+      </Form>
+    </>
   );
 };
 
