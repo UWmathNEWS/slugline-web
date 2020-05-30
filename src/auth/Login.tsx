981e2230353c33be429d3e1e8dc6c95eabc79426
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-05-29 23:13:41 -0400

    Fix login page to handle new authprovider

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -25,21 +25,20 @@ const Login: React.FC = () => {
 
   const onSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    auth.login(username, password).then(
-      (user) => {
+    auth.login(username, password).then((resp) => {
+      if (resp.success) {
         history.push("/");
         toast.addToasts([
           {
             id: Math.random().toString(),
-            body: `Logged in as ${user?.username}.`,
+            body: `Logged in as ${resp.data?.username}.`,
             delay: 3000,
           },
         ]);
-      },
-      (errors: APIError) => {
-        setErrors(errors.detail || []);
+      } else {
+        setErrors(resp.error.detail || []);
       }
-    );
+    });
   };
 
   return (
