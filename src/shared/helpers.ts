d85b401e8ad9f534d8bfacaa31cafc479f1e3cb4
Terry Chen <tyxchen@outlook.com>
2020-06-14 17:55:09 -0400

    Consolidate helper modules into a single file

--- src/shared/helpers.ts
+++
@@ -1,6 +0,0 @@
-import format from "./helpers/format";
-import makeTitle from "./helpers/makeTitle";
-import renderRoutes from "./helpers/renderRoutes";
-import * as url from "./helpers/url";
-
-export { format, makeTitle, renderRoutes, url };
