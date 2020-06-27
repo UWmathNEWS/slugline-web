a87d1a7317b33562220d682f58b4ec94e6e6cba1
Terry Chen <tyxchen@outlook.com>
2020-06-26 23:53:32 -0400

    SSR rendering passes URLs to loadData function

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -103,7 +103,11 @@ interface RouteBaseProps extends Omit<_RouteProps, "render" | "component"> {
   key?: string | number;
   routeComponent?: React.ComponentType;
   routeProps?: any;
-  loadData?: (args: { params: any; headers: any }) => Promise<APIResponse<any>>;
+  loadData?: (args: {
+    url: string;
+    params: any;
+    headers: any;
+  }) => Promise<APIResponse<any>>;
 }
 
 export type RouteProps = RouteBaseProps &
