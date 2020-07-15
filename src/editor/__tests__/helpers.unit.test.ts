99db2f18a55ef205de9972b3aac8a6c5fdf10a5a
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-07-15 19:42:28 -0400

    Add toggleBlock tests

--- src/editor/__tests__/helpers.unit.test.ts
+++ src/editor/__tests__/helpers.unit.test.ts
@@ -1,6 +1,12 @@
-import { createInline, increaseStress, increaseEmph } from "../helpers";
+import {
+  createInline,
+  increaseStress,
+  increaseEmph,
+  toggleBlock,
+} from "../helpers";
 import createCustomEditor from "../CustomEditor";
 import { LinkElement, ElementType, InlineLatexElement, Mark } from "../types";
+import { Transforms, Editor } from "slate";
 
 const TEST_LINK: LinkElement = {
   href: "www.test.com",
@@ -594,6 +600,295 @@ describe("increaseStress", () => {
         children: [
           {
             text: "voodoovoodoo",
+          },
+        ],
+      },
+    ]);
+  });
+});
+
+describe("toggleBlock", () => {
+  it("changes the block type", () => {
+    const editor = createCustomEditor();
+    editor.children = [
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "fish",
+          },
+        ],
+      },
+    ];
+    editor.selection = {
+      anchor: {
+        path: [0, 0],
+        offset: 0,
+      },
+      focus: {
+        path: [0, 0],
+        offset: 0,
+      },
+    };
+
+    toggleBlock(editor, ElementType.Header);
+
+    expect(editor.children).toEqual([
+      {
+        type: ElementType.Header,
+        children: [
+          {
+            text: "fish",
+          },
+        ],
+      },
+    ]);
+  });
+
+  it("toggles multiple blocks", () => {
+    const editor = createCustomEditor();
+    editor.children = [
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "fish",
+          },
+        ],
+      },
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "sticks",
+          },
+        ],
+      },
+    ];
+
+    editor.selection = {
+      anchor: {
+        path: [0, 0],
+        offset: 0,
+      },
+      focus: {
+        path: [1, 0],
+        offset: 6,
+      },
+    };
+
+    toggleBlock(editor, ElementType.Header);
+
+    expect(editor.children).toEqual([
+      {
+        type: ElementType.Header,
+        children: [
+          {
+            text: "fish",
+          },
+        ],
+      },
+      {
+        type: ElementType.Header,
+        children: [
+          {
+            text: "sticks",
+          },
+        ],
+      },
+    ]);
+  });
+
+  it("handles inlines", () => {
+    const editor = createCustomEditor();
+
+    editor.children = [
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "This is a ",
+          },
+          {
+            type: ElementType.Link,
+            href: "http://google.com",
+            children: [
+              {
+                text: "link",
+              },
+            ],
+          },
+          {
+            text: "!",
+          },
+        ],
+      },
+    ];
+
+    editor.selection = {
+      anchor: {
+        path: [0, 0],
+        offset: 0,
+      },
+      focus: {
+        path: [0, 2],
+        offset: 1,
+      },
+    };
+
+    toggleBlock(editor, ElementType.Header);
+
+    expect(editor.children).toEqual([
+      {
+        type: ElementType.Header,
+        children: [
+          {
+            text: "This is a ",
+          },
+          {
+            type: ElementType.Link,
+            href: "http://google.com",
+            children: [
+              {
+                text: "link",
+              },
+            ],
+          },
+          {
+            text: "!",
+          },
+        ],
+      },
+    ]);
+
+    toggleBlock(editor, ElementType.Header);
+
+    expect(editor.children).toEqual([
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "This is a ",
+          },
+          {
+            type: ElementType.Link,
+            href: "http://google.com",
+            children: [
+              {
+                text: "link",
+              },
+            ],
+          },
+          {
+            text: "!",
+          },
+        ],
+      },
+    ]);
+  });
+
+  it("disables the block if any blocks are present", () => {
+    const editor = createCustomEditor();
+    editor.children = [
+      {
+        type: ElementType.Header,
+        children: [
+          {
+            text: "Header",
+          },
+        ],
+      },
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "Not Header",
+          },
+        ],
+      },
+    ];
+
+    editor.selection = {
+      anchor: {
+        path: [0, 0],
+        offset: 0,
+      },
+      focus: {
+        path: [1, 0],
+        offset: 10,
+      },
+    };
+
+    toggleBlock(editor, ElementType.Header);
+
+    expect(editor.children).toEqual([
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "Header",
+          },
+        ],
+      },
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "Not Header",
+          },
+        ],
+      },
+    ]);
+  });
+
+  it("overrides existing blocks", () => {
+    const editor = createCustomEditor();
+    editor.children = [
+      {
+        type: "overwrite-me",
+        children: [
+          {
+            text: "Header",
+          },
+        ],
+      },
+      {
+        type: ElementType.Default,
+        children: [
+          {
+            text: "Not Header",
+          },
+        ],
+      },
+    ];
+
+    editor.selection = {
+      anchor: {
+        path: [0, 0],
+        offset: 0,
+      },
+      focus: {
+        path: [1, 0],
+        offset: 10,
+      },
+    };
+
+    toggleBlock(editor, ElementType.Header);
+
+    expect(editor.children).toEqual([
+      {
+        type: ElementType.Header,
+        children: [
+          {
+            text: "Header",
+          },
+        ],
+      },
+      {
+        type: ElementType.Header,
+        children: [
+          {
+            text: "Not Header",
           },
         ],
       },
