a16f79d8e11a1359e231cfc28066b0bf988ce449
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-08-11 16:00:01 -0400

    On second thought, maybe just use shift+enter for softbreak and avoid all this nonsense

--- src/editor/CustomEditor.ts
+++ src/editor/CustomEditor.ts
@@ -1,12 +1,5 @@
-import { createEditor, Element, Text, Transforms, Editor, Path } from "slate";
-import {
-  SluglineElement,
-  InlineElementType,
-  ElementType,
-  Mark,
-  BlockElementType,
-} from "./types";
-import { isIterableEmpty } from "./helpers";
+import { createEditor, Element, Text, Transforms } from "slate";
+import { SluglineElement, InlineElementType, Mark } from "./types";
 
 // the way to remove a property is to call setNodes with the property set to null
 // so we create a object with all marks set to null so we can remove them all at once
@@ -14,13 +7,10 @@ const REMOVE_ALL_MARKS_OBJ = Object.fromEntries(
   Object.values(Mark).map((mark) => [mark, null])
 );
 
-// Any element in this list will create a soft break on ENTER, instead of creating a new block
-export const SOFT_BREAK_ELEMENTS: ElementType[] = [BlockElementType.Code];
-
 const createCustomEditor = () => {
   const editor = createEditor();
 
-  const { addMark, insertBreak } = editor;
+  const { addMark } = editor;
 
   const isInline = (element: Element) => {
     const e = element as SluglineElement;
@@ -52,27 +42,9 @@ const createCustomEditor = () => {
     return addMark(key, value);
   };
 
-  const insertBreakCustom = () => {
-    if (
-      editor.selection &&
-      Path.equals(editor.selection.anchor.path, editor.selection.focus.path)
-    ) {
-      const nodes = Editor.nodes(editor, {
-        match: (elem) =>
-          SOFT_BREAK_ELEMENTS.includes((elem as SluglineElement).type),
-      });
-      if (!isIterableEmpty(nodes)) {
-        editor.insertText("\n");
-        return;
-      }
-    }
-    insertBreak();
-  };
-
   editor.isInline = isInline;
   editor.isVoid = isVoid;
   editor.addMark = addMarkMutuallyExclusive;
-  editor.insertBreak = insertBreakCustom;
   return editor;
 };
 
