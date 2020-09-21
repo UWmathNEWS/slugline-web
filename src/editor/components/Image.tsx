1e5a85b9994f1b8bbe7e8d7c8b8902b87b55f095
Kevin Trieu <kevin.trieu@setter.com>
2020-09-21 10:12:28 -0400

    Add some styling to the image component

--- src/editor/components/Image.tsx
+++ src/editor/components/Image.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { RenderElementProps } from "slate-react";
+import { RenderElementProps, useFocused, useSelected } from "slate-react";
 import { ImageElement } from "../types";
+
+import "./styles/Image.scss";
 
 const Image: React.FC<RenderElementProps> = (props) => {
   const element = props.element as ImageElement;
+  const selected = useSelected();
+  const focused = useFocused();
+
   return (
-    <div {...props.attributes}>
+    <div className="Image" {...props.attributes}>
       <div contentEditable={false}>
-        <img src={element.src} alt="mathNEWS content" />
+        <img
+          className={`Image__img ${
+            selected && focused ? "Image__img--selected" : ""
+          }`}
+          src={element.src}
+          alt="mathNEWS content"
+        />
       </div>
       {props.children}
     </div>
