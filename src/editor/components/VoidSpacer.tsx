a97d1a75ceb89f15d76d1e81f2312cb21472c25b
Kevin Trieu <kevin.trieu@setter.com>
2020-10-18 18:01:41 -0400

    Make void spacer selection match existing selection

--- src/editor/components/VoidSpacer.tsx
+++ src/editor/components/VoidSpacer.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { RenderElementProps, useSelected } from "slate-react";
 
+import "./styles/VoidSpacer.scss";
+
 const VoidSpacer: React.FC<RenderElementProps> = (props) => {
   const selected = useSelected();
   return (
     <span
       {...props.attributes}
-      style={{
-        display: "block",
-        background: selected ? "red" : "none",
-        width: "100%",
-        height: "2px",
-      }}
+      className={`VoidSpacer ${selected && "VoidSpacer--selected"}`}
     >
       {props.children}
     </span>
